72904d99bd2deff90b4f0aab2fb755914cc4d75a
cem <cemclk.jolley@gmail.com>
2026-01-01 16:46:30 +0000

    further work on sql testing, some experimental functions added, internal structure of Database object changed.

--- test/sql.py
+++ test/sql.py
@@ -4,47 +4,53 @@ class Database:
     '''Database class for managing SQLite database connections and operations.'''
 
     #set database configurations
-    name = "app.db"
-    initial_tables = []
-    connection = create_connection()
+    name = "standart"
+    connected = False
+    column_types = ["NULL", "INTEGER", "REAL", "TEXT", "BLOB"]
+    registered_columns = []
 
 
     def create_connection(self):
-        try:
-            conn = sqlite3.connect(self.name)
-            self.connected = True
-            return conn
+        conn = sqlite3.connect(f"{self.name}.db")
+        self.connected = True
         
-        except sqlite3.Error as e:
-            self.connected = False
-            return f"Error connecting to database:\n___\n {e}"
-            
+        return conn
     
-
-    def create_table(self, table_name, columns=None):
-        if self.connection is False:
+    
+    #create table with given column specifics
+    def create_table(self, table_name, columns = None):
+        
+        if self.connected is False:
             return "No database connection established."
 
-        conn = self.connection
-        cursor = conn.cursor()
-        res = cursor.execute(f'''CREATE TABLE IF NOT EXISTS {table_name} ({columns})''').commit()
-        
-        return res
-        
-        
-    def create_tables(self, tables):
-        for table_name, columns in tables.items():
-            self.create_table(table_name, columns)
-
-    def set_data(self, table_name, data):
-        pass
-
-    def get_data(self, table_name, conditions=None):
-        pass
+        with self.create_connection() as conn:
+            cursor = conn.cursor()
+            if columns:
+                response = cursor.execute(f'CREATE TABLE IF NOT EXISTS {table_name} ({", ".join(columns)})').commit()
+            else:
+                response = cursor.execute(f'CREATE TABLE IF NOT EXISTS {table_name} ({", ".join(self.registered_columns)})').commit()
+            
+            return response
     
-    def update_data(self, table_name, updates, conditions=None):
-        pass
 
-    def delete_data(self, table_name, conditions=None):
-        pass
 
+    def register_column(self, column_name, type, not_null= True, primary_key=False, unique = False, autoinc = False):
+        
+        if type.lower() not in [item.lower() for item in self.column_types]:
+            return "invalid column type, you may define the columns: {', '.join(self.column_types)} "
+
+        column_statement = f"{column_name} "
+        
+        if not_null:
+            column_statement + "NOT NULL"
+        
+        if primary_key:
+            column_statement + "PRIMARY KEY"
+        
+        if autoinc:
+            column_statement + "AUTOINCREMENT"
+
+        if unique:
+            column_statement + "UNIQUE"        
+
+        return column_statement
